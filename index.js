f4ccfc48813afc6749432801ccf12ed392e37c3c
Kevin Rogers <kevinarogers1@gmail.com>
2021-12-02 16:25:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const employee = {
-  name: 'fullName', 
-  streetAddress: 'street',
-}
+const employee = {                        //template for which employees will be defined 
+  name: "fullName" ,
+  streetAddress: "line1" ,
+};
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-  return {
-  ...employee,
-  [key]: value,
+  return {                                    
+   ...employee,                               //Spread operator clones a copy of employee template
+   [key]: value,                              //key value is assigned to our cloned object template
+  
   };
+  
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-   employee[key] = value;
-   return employee;
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+
+  return employee;
 }
 
 function deleteFromEmployeeByKey(employee, key) {
-  const cloneEmployee = {...employee };
-  
-  delete cloneEmployee[key]
-   
-  return cloneEmployee;
-} 
-  
+  const clonedEmployee = { ...employee };
+
+  delete clonedEmployee[key];
+
+  return clonedEmployee;
+}
+
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
   delete employee[key];
+
   return employee;
-} 
+}
+
